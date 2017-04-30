a947479b3cd12504b2822a51c2ec8a4e010efe47
Pavel Suk <pavelsuk@users.noreply.github.com>
2017-04-30 20:16:55 +0200

    Emitting events

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
   });
 });
 
